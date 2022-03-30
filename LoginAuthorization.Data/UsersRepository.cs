32cc5d9f732575a3b0903eb1e95b49f74e4b0287
Esther Turin <turinesther@gmail.com>
2022-03-30 00:19:22 -0400

    Add project files.

---
+++ LoginAuthorization.Data/UsersRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAuthorization.Data
+{
+    public class UserRepository
+    {
+        private string _connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddUser(User user, string password)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO Users (Name, Email, PasswordHash) " +
+                "VALUES (@name, @email, @hash)";
+            cmd.Parameters.AddWithValue("@name", user.Name);
+            cmd.Parameters.AddWithValue("@email", user.Email);
+            cmd.Parameters.AddWithValue("@hash", BCrypt.Net.BCrypt.HashPassword(password));
+
+            connection.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        public User Login(string email, string password)
+        {
+            var user = GetByEmail(email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            bool isValid = BCrypt.Net.BCrypt.Verify(password, user.Password);
+            return isValid ? user : null;
+
+        }
+
+        public User GetByEmail(string email)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT TOP 1 * FROM Users WHERE Email = @email";
+            cmd.Parameters.AddWithValue("@email", email);
+            connection.Open();
+            var reader = cmd.ExecuteReader();
+            if (!reader.Read())
+            {
+                return null;
+            }
+
+            return new User
+            {
+                Id = (int)reader["Id"],
+                Email = (string)reader["Email"],
+                Name = (string)reader["Name"],
+                Password = (string)reader["PasswordHash"]
+            };
+        }
+    }
+
+}
+
+
